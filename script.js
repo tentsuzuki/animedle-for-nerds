0410e62060484f7753387b1a36d2c95526b74a7e
tentsuzuki <127833526+tentsuzuki@users.noreply.github.com>
2023-07-07 14:34:07 -0700

    added a basic dropdown with titles

--- script.js
+++ script.js
@@ -1,55 +1,84 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const guessInput = document.getElementById("guess");
-    const guessButton = document.getElementById("button");
-    const result = document.getElementById("result");
-    const animeImage = document.getElementById("image");
-  
-    let animeTitle = "";
-    let animeImageUrl = "";
-    let attempts = 0;
-    const maxAttempts = 5;
-  
-    guessButton.addEventListener("click", checkGuess);
-  
-    function checkGuess() {
-      const guess = guessInput.value.trim().toLowerCase();
-      guessInput.value = "";
-  
-      if (guess === animeTitle) {
-        result.textContent = "go outside please";
-        guessButton.disabled = true;
-      } else {
-        attempts++;
-        if (attempts >= maxAttempts) {
-          result.textContent = `wow ur bad. title: ${animeTitle}`;
-          guessButton.disabled = true;
-        } else {
-          result.textContent = `guess harder: attempt ${attempts}/${maxAttempts}`;
-        }
-      }
+const guessInput = document.getElementById("guess");
+const guessButton = document.getElementById("button");
+const result = document.getElementById("result");
+const animeImage = document.getElementById("image");
+const dropdown = document.getElementById("dropdown");
+
+let animeTitle = "";
+let animeImageUrl = "";
+let attempts = 0;
+const maxAttempts = 5;
+const animeData = [];
+
+guessButton.addEventListener("click", checkGuess);
+guessInput.addEventListener("input", showDropdown);
+
+function checkGuess() {
+  const guess = guessInput.value.trim().toLowerCase();
+  guessInput.value = "";
+
+  if (guess === animeTitle) {
+    result.textContent = "go outside, please.";
+    guessButton.disabled = true;
+  } else {
+    attempts++;
+    if (attempts >= maxAttempts) {
+      result.textContent = `wow, you're bad. the title is: ${animeTitle}`;
+      guessButton.disabled = true;
+    } else {
+      result.textContent = `guess harder: attempt ${attempts}/${maxAttempts}`;
     }
-  
-    fetch('anime.json')
-      .then(response => response.json())
-      .then(data => {
-        let randomAnime = getRandomAnime(data.data);
-  
-        animeTitle = randomAnime.title;
-        animeImageUrl = randomAnime.picture;
-        updateAnimeImage();
-      })
-      .catch(error => {
-        console.log(error);
-        result.textContent = "something wrong happened";
-      });
-  
-    function updateAnimeImage() {
-      animeImage.src = animeImageUrl;
-    }
-  
-    function getRandomAnime(animeData) {
-      const randomIndex = Math.floor(Math.random() * animeData.length);
-      return animeData[randomIndex];
-    }
+  }
+}
+
+function showDropdown() {
+  const searchText = guessInput.value.trim().toLowerCase();
+  const matchingTitles = animeData.filter((title) =>
+    title.toLowerCase().startsWith(searchText)
+  );
+
+  if (searchText === "") {
+    dropdown.innerHTML = "";
+    dropdown.classList.remove("show");
+  } else {
+    const dropdownItems = matchingTitles
+      .map(
+        (title) =>
+          `<div class="dropdown-item">${title}</div>`
+      )
+      .join("");
+    dropdown.innerHTML = dropdownItems;
+    dropdown.classList.add("show");
+  }
+}
+
+dropdown.addEventListener("click", (event) => {
+  const selectedTitle = event.target.textContent;
+  guessInput.value = selectedTitle;
+  dropdown.innerHTML = "";
+  dropdown.classList.remove("show");
+});
+
+fetch("anime.json")
+  .then((response) => response.json())
+  .then((data) => {
+    animeData.push(...data.data.map((anime) => anime.title));
+    let randomAnime = getRandomAnime(data.data);
+
+    animeTitle = randomAnime.title;
+    animeImageUrl = randomAnime.picture;
+    updateAnimeImage();
+  })
+  .catch((error) => {
+    console.log(error);
+    result.textContent = "Something went wrong.";
   });
-  
+
+function updateAnimeImage() {
+  animeImage.src = animeImageUrl;
+}
+
+function getRandomAnime(animeData) {
+  const randomIndex = Math.floor(Math.random() * animeData.length);
+  return animeData[randomIndex];
+}
